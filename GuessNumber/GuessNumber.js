72497a2cc6c4c84a37fad2f54eb31b68c9a05561
LuisFRLima <luis.felipe@rcatechnology.com.br>
2021-07-13 15:47:48 -0300

    Jogo faltando o timer

--- GuessNumber/GuessNumber.js
+++ GuessNumber/GuessNumber.js
@@ -1,44 +1,88 @@
+const mensagens = require("./variaveis");
 const readline = require("readline");
+
+
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-var dificuldade = "\nEscolha uma dificuldade: \n 1 - Fácil \n 2 - Médio \n 3 - Difícil \n 4 - Insano \n 5 - Impossível\n"
-var boasVindas = "Boas Vindas Caro Jogador, ao meu jogo de Adivinhação, Sou o Gênio Sinep O Grande";
-var regras = "\nO jogo é relativamente simples tente adivinhar o número que estou pensando! Caso acerte prêmios lhe agurdam";
-var aviso = "\nPara começar este jogo escolha uma dificuldade digitando o número correspondente e aperte a tecla ENTER";
-var avisoFacil = "Vejo que não tem muita confiaça em sua habilidade,então irei pegar leve";
-var startFacil = "\nTente adivinhar o número que pensei, digite um número entre 0 e 100";
-var numeroPensado = "Que número pensei? ";
-var mensagemFinal = "Parabéns você acertou!!! Seu Prêmio é Uma nota de 3 Reais"
-console.log(boasVindas,regras,aviso);
 
-rl.question(dificuldade, function(numeroDificuldade) 
-    {
-        console.log(avisoFacil,startFacil);
-        aleatorio(0, 100);
-        console.log(aleatorio(0, 100));
-        while(aleatorio != numeroChutado)
-        {
-            rl.question(numeroPensado, function(numeroChutado) 
-            {
-                console.log(mensagemFinal);
-                rl.close(); 
-            });
-        }    
-});
+console.log(mensagens.boasVindas,mensagens.regras,mensagens.aviso);
+rl.question(mensagens.dificuldade, async function(x){await perguntinha(x)});
 
 rl.on("close", function() 
 {
-    console.log("\nFim de Jogo!!!");
+    console.log("\n !!!Fim de Jogo!!!");
     process.exit(0);
 });
 
 
-function aleatorio(min, max) 
+async function aleatorio(min, max) 
 {  
     return Math.floor
     (
       Math.random() * (max - min + 1) + min
     )
 }
+async function perguntinha(numeroDificuldade) 
+{
+    if(isNaN(numeroDificuldade) || numeroDificuldade < 1 || numeroDificuldade > 5)
+    {
+        console.log("Digite apenas o valor da dificuldade"); 
+        rl.question(mensagens.dificuldade, async function(x){await perguntinha(x)}); 
+        return; 
+    }
+        switch(numeroDificuldade)
+        {   
+            case "1": 
+                await numeroGerado(await aleatorio(0, 100))
+                console.log(mensagens.avisoFacil,mensagens.startFacil,mensagens.numeroPensado);
+                break;
+            case "2": 
+                await numeroGerado(await aleatorio(0, 1000));
+                console.log(mensagens.avisoMedio,mensagens.startMedio,mensagens.numeroPensado);
+                break;
+            case "3": 
+                await numeroGerado(await aleatorio(0, 999999));
+                console.log(mensagens.avisoDificil,mensagens.startDificil,mensagens.numeroPensado);
+                break;
+            case "4": 
+                await numeroGerado(await aleatorio(0, 2345987));
+                console.log(mensagens.avisoInsano,mensagens.startInsano,mensagens.numeroPensado);
+                break;
+            case "5": 
+                await numeroGerado(await aleatorio(-2345987, 2345999));
+                console.log(mensagens.avisoImpossivel,mensagens.startImpossivel,mensagens.numeroPensado);
+                break;
+            default: 
+                console.log("Valor Invalido");
+                break; 
+        }    
+}
+async function numeroGerado(resposta)
+{
+    rl.question("", x => respostinha(x , resposta));
+     
+} 
+async function respostinha(numeroChutado,resposta)
+{
+    if(numeroChutado == resposta)
+    {
+        console.log(mensagens.mensagemFinal);
+        rl.close();
+    }
+    else if(numeroChutado > resposta)
+    {       
+         await maior(resposta);     
+    }
+    else await menor(resposta);    
+} 
+async function menor(resposta)
+{
+    rl.question(mensagens.dicasMenor, x => respostinha(x , resposta)); 
+}  
+async function maior(resposta)
+{
+    rl.question(mensagens.dicasMaior, x => respostinha(x , resposta)); 
+}  
---
+++ GuessNumber/variaveis.js
@@ -0,0 +1,21 @@
+module.exports =
+{
+    dificuldade: "\nEscolha uma dificuldade: \n 1 - Fácil \n 2 - Médio \n 3 - Difícil \n 4 - Insano \n 5 - Impossível\n",
+    boasVindas: "Boas Vindas Caro Jogador, ao meu jogo de Adivinhação, Sou o Gênio Sinep O Grande",
+    regras: "\nO jogo é relativamente simples tente adivinhar o número que estou pensando! Caso acerte prêmios lhe agurdam",
+    aviso: "\nPara começar este jogo escolha uma dificuldade digitando o número correspondente e aperte a tecla ENTER",
+    avisoFacil: "\nVejo que não tem confiaça em sua habilidade,então não irei dificultar",
+    startFacil: "\nTente adivinhar o número que pensei, digite um número entre 0 e 100",
+    avisoMedio: "\nVejo que não tem muita confiaça em sua habilidade, então irei pegar leve",
+    startMedio: "\nTente adivinhar o número que pensei, digite um número entre 0 e 1000",
+    avisoDificil: "\nVejo que mostra um pouco de confiança em sua habilidade,vamos testar-la",
+    startDificl: "\nTente adivinhar o número que pensei, digite um número entre 0 e 999999",
+    avisoInsano: "\nVejo que busca um dasafio",
+    startInsano: "\nTente adivinhar o número que pensei, digite um número entre 0 e 2345987",
+    avisoImpossivel: "\nVejo que buscar me superar, agora tente",
+    startInsano: "\nTente adivinhar o número que pensei, digite um número entre -2345987 e 2345987",
+    numeroPensado: "\nQue número pensei: ?",
+    mensagemFinal: "Parabéns você acertou!!! Seu Prêmio é Uma nota de 3 Reais",
+    dicasMaior: "Sua habilidade de adivinhação está muito alta, diminua se quiser o prêmio\n",
+    dicasMenor:"Sua habilidade de adivinhação está muito baixa, aumente se quiser o prêmio\n"
+}
